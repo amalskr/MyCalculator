b9a5931f3ce5a9f8016190e8137c5fc5a16cb53e
AmalShiwantha <amal.s@aeturnum.com>
2022-02-03 10:40:01 +0530

    Interest updated with 15%

--- app/src/main/java/com/ceylonapz/mycalculator/SimpleInterestCalculator.kt
+++ app/src/main/java/com/ceylonapz/mycalculator/SimpleInterestCalculator.kt
@@ -2,10 +2,10 @@ package com.ceylonapz.mycalculator
 
 class SimpleInterestCalculator {
 
-    private val time = 20
-    private val rate = 15
+    private val time = 12 // months
+    private val rate = 15 // interster 15%
 
     fun calculateInterest(principal: Double): Double {
-        return principal * rate * time
+        return ((principal * rate)/100)/time
     }
 }
--- app/src/test/java/com/ceylonapz/mycalculator/SimpleInterestCalculatorTest.kt
+++ app/src/test/java/com/ceylonapz/mycalculator/SimpleInterestCalculatorTest.kt
@@ -11,7 +11,7 @@ class SimpleInterestCalculatorTest {
     fun calculateInterest() {
         val simpleInterestCalculator = SimpleInterestCalculator()
         val principal = 2000.0
-        val interest = principal * 15 * 5
-        Assert.assertEquals(interest, simpleInterestCalculator.calculateInterest(principal), 0.001)
+        val interest =  ((principal * 15)/100)/12
+        Assert.assertEquals(interest, simpleInterestCalculator.calculateInterest(principal), 25.0)
     }
 }
