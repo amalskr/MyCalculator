a36992c7c48aeaba1aa5df3134883b327a612dc0
AmalShiwantha <amal.s@aeturnum.com>
2022-02-03 10:26:01 +0530

    updated calcuate time

--- app/src/main/java/com/ceylonapz/mycalculator/SimpleInterestCalculator.kt
+++ app/src/main/java/com/ceylonapz/mycalculator/SimpleInterestCalculator.kt
@@ -2,7 +2,7 @@ package com.ceylonapz.mycalculator
 
 class SimpleInterestCalculator {
 
-    private val time = 5
+    private val time = 20
     private val rate = 15
 
     fun calculateInterest(principal: Double): Double {
